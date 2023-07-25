8c84056497a776c1a1e6d64384e60596ab6ba62e
Jonatas Castelo Branco <101611564+Jonatas9204@users.noreply.github.com>
2023-07-25 18:37:00 -0300

    Add files via upload

--- Auditoria.sql
+++ Auditoria.sql
@@ -1,7 +1,7 @@
 -- CRIANDO TRIGGER DE AUDITORIA PARA IDENTIFICAR ALTERAÇÕES NOS VALORES ORÇAMENTÁRIOS PREVIAMENTE 
 -- CADASTRADOS, AFIM DE MAIOR SEGURANÇA E CONSISTÊNCIA DOS DADOS.
 
-DROP TABLE IF EXISTS AUDITORIA_PIP;
+DROP TABLE AUDITORIA_PIP;
 CREATE TABLE auditoria_PIP (
 	id_orcamentario INT ,
   id_projeto2 INT,
@@ -19,9 +19,37 @@ CREATE TABLE auditoria_PIP (
 	updatedat datetime not null,
 	operation char(3) not null,
 	usuario varchar (20),
+    old_row_data JSON,
+    new_row_data JSON,
+	CHECK( operation = 'INS' or operation = 'DEL')
+	);CREATE TABLE auditoria_PIP (
+	id_orcamentario INT ,
+  id_projeto2 INT,
+  uo_cod VARCHAR(45),
+  fonte_1 VARCHAR(45),
+  fonte_2 VARCHAR(45),
+  fonte_3 VARCHAR(45),
+  fonte_4 VARCHAR(45),
+  CAIXA_2022 VARCHAR(45),
+  CAIXA_2023 VARCHAR(45),
+  CAIXA_2024 VARCHAR(45),
+  DEMAIS_FONTES_2022 VARCHAR(45),
+  DEMAIS_FONTES_2023 VARCHAR(45),
+  DEMAIS_FONTES_2024 VARCHAR(45),
+	updatedat datetime not null,
+	operation char(3) not null,
+	usuario varchar (20),
+    old_row_data JSON,
+    new_row_data JSON,
 	CHECK( operation = 'INS' or operation = 'DEL')
 	);
-
+    
+    
+    
+--
+-- TRIGGER DE INSERT
+--
+    
 DELIMITER $$
 CREATE TRIGGER auditoria_orcamento AFTER INSERT 
 ON tb_act_orcamentario
@@ -43,7 +71,9 @@ BEGIN
         DEMAIS_FONTES_2024,
         updatedat,
         usuario,
-        operation
+        operation,
+        old_row_data, -- Adicionando a coluna old_row_data
+        new_row_data  -- Adicionando a coluna new_row_data
     )
     VALUES (
         NEW.id_orcamentario,
@@ -61,10 +91,30 @@ BEGIN
         NEW.DEMAIS_FONTES_2024,
         NOW(),
         USER(),
-        'INS'
+        'INS',
+        JSON_OBJECT(
+            'id_orcamentario', NEW.id_orcamentario,
+            'id_projeto2', NEW.id_projeto2,
+            'uo_cod', NEW.uo_cod,
+            'fonte_1', NEW.fonte_1,
+            'fonte_2', NEW.fonte_2,
+            'fonte_3', NEW.fonte_3,
+            'fonte_4', NEW.fonte_4,
+            'CAIXA_2022', NEW.CAIXA_2022,
+            'CAIXA_2023', NEW.CAIXA_2023,
+            'CAIXA_2024', NEW.CAIXA_2024,
+            'DEMAIS_FONTES_2022', NEW.DEMAIS_FONTES_2022,
+            'DEMAIS_FONTES_2023', NEW.DEMAIS_FONTES_2023,
+            'DEMAIS_FONTES_2024', NEW.DEMAIS_FONTES_2024
+        ),
+        NULL -- Como é uma inserção, a coluna old_row_data será NULL
     );
 END$$
 DELIMITER ;
+
+--
+-- TRIGGER DE DELETE
+--
 
 DELIMITER $$
 CREATE TRIGGER auditoria_orcamento2 AFTER DELETE
@@ -87,7 +137,9 @@ BEGIN
         DEMAIS_FONTES_2024,
         updatedat,
         usuario,
-        operation
+        operation,
+        old_row_data, -- Adicionando a coluna old_row_data
+        new_row_data  -- Adicionando a coluna new_row_data
     )
     VALUES (
         OLD.id_orcamentario,
@@ -105,10 +157,27 @@ BEGIN
         OLD.DEMAIS_FONTES_2024,
         NOW(),
         USER(),
-        'DEL'
+        'DEL',
+        JSON_OBJECT(
+            'id_orcamentario', OLD.id_orcamentario,
+            'id_projeto2', OLD.id_projeto2,
+            'uo_cod', OLD.uo_cod,
+            'fonte_1', OLD.fonte_1,
+            'fonte_2', OLD.fonte_2,
+            'fonte_3', OLD.fonte_3,
+            'fonte_4', OLD.fonte_4,
+            'CAIXA_2022', OLD.CAIXA_2022,
+            'CAIXA_2023', OLD.CAIXA_2023,
+            'CAIXA_2024', OLD.CAIXA_2024,
+            'DEMAIS_FONTES_2022', OLD.DEMAIS_FONTES_2022,
+            'DEMAIS_FONTES_2023', OLD.DEMAIS_FONTES_2023,
+            'DEMAIS_FONTES_2024', OLD.DEMAIS_FONTES_2024
+        ),
+        NULL -- Como é uma exclusão, a coluna new_row_data será NULL
     );
 END$$
 DELIMITER ;
+
 
 -- ------------------------------------------------
  -- TESTANDO AS TRIGGERS DE INSERT INTO E DELETE.
@@ -129,7 +198,7 @@ DELIMITER ;
   DEMAIS_FONTES_2023,
   DEMAIS_FONTES_2024
 ) VALUES (
-  1180,
+  1181,
   1182,
   '3555',
   '0101000999',
@@ -144,6 +213,6 @@ DELIMITER ;
   '1265000.00'
 );
 
-DELETE FROM tb_act_orcamentario WHERE (ID_ORCAMENTARIO = '1180');
+DELETE FROM tb_act_orcamentario WHERE (ID_ORCAMENTARIO = '1181');
   
 SELECT * FROM auditoria_pip;
