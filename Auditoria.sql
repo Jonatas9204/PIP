6276a90a2e79f26ab6dd94a3b2cb36ccf1e82443
Jonatas Castelo Branco <101611564+Jonatas9204@users.noreply.github.com>
2023-07-26 17:24:06 -0300

    Add files via upload

--- Auditoria.sql
+++ Auditoria.sql
@@ -25,27 +25,7 @@ CREATE TABLE auditoria_PIP (
 	usuario varchar (20),
     old_row_data JSON,
     new_row_data JSON,
-	CHECK( operation = 'INS' or operation = 'DEL')
-	);CREATE TABLE auditoria_PIP (
-	id_orcamentario INT ,
-  id_projeto2 INT,
-  uo_cod VARCHAR(45),
-  fonte_1 VARCHAR(45),
-  fonte_2 VARCHAR(45),
-  fonte_3 VARCHAR(45),
-  fonte_4 VARCHAR(45),
-  CAIXA_2022 VARCHAR(45),
-  CAIXA_2023 VARCHAR(45),
-  CAIXA_2024 VARCHAR(45),
-  DEMAIS_FONTES_2022 VARCHAR(45),
-  DEMAIS_FONTES_2023 VARCHAR(45),
-  DEMAIS_FONTES_2024 VARCHAR(45),
-	updatedat datetime not null,
-	operation char(3) not null,
-	usuario varchar (20),
-    old_row_data JSON,
-    new_row_data JSON,
-	CHECK( operation = 'INS' or operation = 'DEL')
+	CHECK (operation IN ('INS', 'DEL', 'UPD'))
 	);
     
     
@@ -220,3 +200,88 @@ DELIMITER ;
 DELETE FROM tb_act_orcamentario WHERE (ID_ORCAMENTARIO = '1181');
   
 SELECT * FROM auditoria_pip;
+
+
+
+--
+-- TRIGGER DE UPDATE
+--
+
+DELIMITER $$
+CREATE TRIGGER auditoria_orcamento_update AFTER UPDATE
+ON tb_act_orcamentario
+FOR EACH ROW
+BEGIN
+    INSERT INTO auditoria_PIP (
+        id_orcamentario,
+        id_projeto2,
+        uo_cod,
+        fonte_1,
+        fonte_2,
+        fonte_3,
+        fonte_4,
+        CAIXA_2022,
+        CAIXA_2023,
+        CAIXA_2024,
+        DEMAIS_FONTES_2022,
+        DEMAIS_FONTES_2023,
+        DEMAIS_FONTES_2024,
+        updatedat,
+        usuario,
+        operation,
+        old_row_data,
+        new_row_data
+    )
+    VALUES (
+        NEW.id_orcamentario,
+        NEW.id_projeto2,
+        NEW.uo_cod,
+        NEW.fonte_1,
+        NEW.fonte_2,
+        NEW.fonte_3,
+        NEW.fonte_4,
+        NEW.CAIXA_2022,
+        NEW.CAIXA_2023,
+        NEW.CAIXA_2024,
+        NEW.DEMAIS_FONTES_2022,
+        NEW.DEMAIS_FONTES_2023,
+        NEW.DEMAIS_FONTES_2024,
+        NOW(),
+        USER(),
+        'UPD',
+        JSON_OBJECT(
+            'id_orcamentario', OLD.id_orcamentario,
+            'id_projeto2', OLD.id_projeto2,
+            'uo_cod', OLD.uo_cod,
+            'fonte_1', OLD.fonte_1,
+            'fonte_2', OLD.fonte_2,
+            'fonte_3', OLD.fonte_3,
+            'fonte_4', OLD.fonte_4,
+            'CAIXA_2022', OLD.CAIXA_2022,
+            'CAIXA_2023', OLD.CAIXA_2023,
+            'CAIXA_2024', OLD.CAIXA_2024,
+            'DEMAIS_FONTES_2022', OLD.DEMAIS_FONTES_2022,
+            'DEMAIS_FONTES_2023', OLD.DEMAIS_FONTES_2023,
+            'DEMAIS_FONTES_2024', OLD.DEMAIS_FONTES_2024
+        ),
+        JSON_OBJECT(
+            'id_orcamentario', NEW.id_orcamentario,
+            'id_projeto2', NEW.id_projeto2,
+            'uo_cod', NEW.uo_cod,
+            'fonte_1', NEW.fonte_1,
+            'fonte_2', NEW.fonte_2,
+            'fonte_3', NEW.fonte_3,
+            'fonte_4', NEW.fonte_4,
+            'CAIXA_2022', NEW.CAIXA_2022,
+            'CAIXA_2023', NEW.CAIXA_2023,
+            'CAIXA_2024', NEW.CAIXA_2024,
+            'DEMAIS_FONTES_2022', NEW.DEMAIS_FONTES_2022,
+            'DEMAIS_FONTES_2023', NEW.DEMAIS_FONTES_2023,
+            'DEMAIS_FONTES_2024', NEW.DEMAIS_FONTES_2024
+        )
+    );
+END$$
+DELIMITER ;
+
+
+
